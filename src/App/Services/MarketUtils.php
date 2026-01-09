fd7d2d78f8721b978fce21973e4fdab8660ef61a
ovidiurosu90 <ovidiuro@ovidiurosu.com>
2026-01-09 12:20:13 +0100

    [positions] Small cosmetic fix for countdown

--- src/App/Services/MarketUtils.php
+++ src/App/Services/MarketUtils.php
@@ -228,7 +228,7 @@ class MarketUtils
             . '" data-bs-toggle="tooltip" data-bs-custom-class="big-tooltips2" '
             . 'data-bs-html="true" title="<p class=\'text-left\'>'
             . $tooltip . '</p>">' . $text
-            . '</span></div> <div class="col-md-auto p-0">' . $countdown . "</div>";
+            . '</span></div> <div class="p-0">' . $countdown . "</div>";
 
         return $output;
     }
